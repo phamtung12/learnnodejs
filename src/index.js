e2cc7613a931ddaf9e4a0660285a4d914e8739a0
Phamtung-IT <150701508+Phamtung-IT@users.noreply.github.com>
2024-08-25 20:13:36 +0700

    second commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require('express')  // expressjs
+const morgan = require('morgan')  // morgan
+const handlebars = require('express-handlebars')  // express-handlebars
+const path = require('path')  // path
+const app = express()
+const port = 3000 // port : 127.0.0.1 & localhost:3000
+
+app.use(morgan('combined')) // use 'combined' of morgan  (HTTP Logger)
+
+// Template engine
+app.engine('handlebars', handlebars.engine())
+app.set('view engine', 'handlebars');
+app.set("views", path.join(__dirname, "resources\\views"));
+
+// Định tuyến route
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/news', (req, res) => {
+    res.render('news')
+})
+
+
+
+app.get('/route', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
